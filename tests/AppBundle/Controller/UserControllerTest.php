3f0d5659e6dd386acffa5837523f7a4cde4be26b
sapphaos <mewapopo@gmail.com>
2019-05-20 14:47:26 +0200

    Ref #17 user edition tests

--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -9,30 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserControllerTest extends WebTestCase
 {
-    /*public function testLogin()
-    {
-        // Create a new client to browse the application
-        $client = static::createClient();
-
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET ");
-
-        // Get the form
-        $form = $crawler->selectButton('Connexion')->form();
-        // Fill the login form input
-        $form['_username']->setValue('admin');
-        $form['_password']->setValue('a');
-        // Send the form
-        $client->submit($form);
-
-        $this->assertContains(
-                'Bienvenue admin !',
-                $client->getResponse()->getContent()
-        );
-    }*/
-
-    /**
+        /**
      * Connecte au site afin de pouvoir naviguer
      */
     public function connection()
@@ -55,27 +32,18 @@ class UserControllerTest extends WebTestCase
         $client = $this->connection();
         $crawler = $client->request('GET', '/user/');
         $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
-
         // Vérifie si la page affiche le bon texte
         $this->assertContains(
                 'Liste des utilisateurices',
                 $client->getResponse()->getContent()
         );
-
         // Sélectionne le bouton d'édition de l'utilisateur créé précédemment
         $link = $crawler
             ->filter('tr > td > a:contains("")')
             ->last()
             ->link()
         ;
-
         $crawler = $client->click($link);
-
-        $this->assertContains(
-                'Profil de Jean',
-                $client->getResponse()->getContent()
-        );
-
         return array($client,$crawler);
     }
 
@@ -123,7 +91,30 @@ class UserControllerTest extends WebTestCase
         //dump($button->html());
     }
 
-    public function testEdit()
+    /**
+     * Change la responsabilité de l'utilisateur créé plus tôt
+     * Lui affecte la reponsabilité Gestionnaire 1
+     */
+    public function testEditResponsability()
+    {
+        $editPage = $this->accessEditPage();
+        $client = $editPage[0];
+        $crawler = $editPage[1];
+
+        $form = $crawler->selectButton('Changer les informations')->form();
+        $form['appbundle_user']['responsabilities'][1]->tick();
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+        // J'essaye de vérifier si c'est checked mais je ne trouve pas... miss tère...
+        $this->assertContains('Les informations ont bien été modifiées',
+                $client->getResponse()->getContent()
+        );
+    }
+
+    /**
+     * Change le pseudonyme
+     */
+    public function testEditPseudo()
     {
         $editPage = $this->accessEditPage();
         $client = $editPage[0];
@@ -131,16 +122,34 @@ class UserControllerTest extends WebTestCase
 
         $form = $crawler->selectButton('Changer les informations')->form();
         $values = $form->getPhpValues();
-        dump($values);
-        $resp = $crawler->filter('.form-group')->eq(1);
-        $blogTitle  = $resp->text();
-        dump($blogTitle);
-        $crawler = $client->click($resp->link());
-        //dump($values['appbundle_user']['_token']);
+        $values['appbundle_user']['username'] = 'René';
+        $crawler = $client->request($form->getMethod(), $form->getUri(), $values,$form->getPhpFiles());
+        $crawler = $client->followRedirect();
+        $this->assertContains('René',
+                $client->getResponse()->getContent()
+        );
+        $this->assertContains('Les informations ont bien été modifiées',
+                $client->getResponse()->getContent()
+        );
+    }
 
-        /*$values['appbundle_user']['username'] = 'Jean';
-        $values['appbundle_user']['plainPassword']['first'] = 'motdepasse';
-        $values['appbundle_user']['plainPassword']['second'] = 'motdepasse';*/
+    /**
+     * Change le mot de passe
+     */
+    public function testEditPassword()
+    {
+        $editPage = $this->accessEditPage();
+        $client = $editPage[0];
+        $crawler = $editPage[1];
 
+        $form = $crawler->selectButton('Changer le mot de passe')->form();
+        $values = $form->getPhpValues();
+        $values['appbundle_password']['oldPassword'] = 'motdepasse';
+        $values['appbundle_password']['plainPassword']['first'] = 'password';
+        $values['appbundle_password']['plainPassword']['second'] = 'password';
+        $crawler = $client->request($form->getMethod(), $form->getUri(), $values,$form->getPhpFiles());
+        $this->assertContains('Le mot de passe a bien été modifié',
+                $client->getResponse()->getContent()
+        );
     }
 }
