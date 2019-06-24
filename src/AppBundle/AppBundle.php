a6e0617dd5860bdf7aaed364821543dd3bb3b75c
Yann-BUTSCHER-EIRL <yann.butscher.eirl@gmail.com>
2019-06-24 11:46:26 +0200

    MINOR : Add phpdoc in order to comply with the phpcs checking.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * @package AppBundle
+ * @version 1.0
+ * @author Lucile-DT
+ * @license GPU v3.0
+ */
 
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Bundle declaration class.
+ */
 class AppBundle extends Bundle
 {
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @package AppBundle\Entity
+ * @version 1.0
+ * @author Lucile-DT
+ * @license GPU v3.0
+ */
 
 namespace AppBundle\Entity;
 
