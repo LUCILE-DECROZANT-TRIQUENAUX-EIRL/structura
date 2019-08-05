6d16865b07566a826cc927787b8c5540ed7600fd
sapphaos <mewapopo@gmail.com>
2019-08-05 17:12:06 +0200

    Ref #82 : Fix edition of roles in profile

---
+++ src/Form/UserRolesType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Responsibility;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Form for user's infos other than their password
+ */
+class UserRolesType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+                ->add('responsibilities', EntityType::class, [
+                    // looks for choices from this entity
+                    'class' => Responsibility::class,
+                    // uses the Responsibility.label property as the visible option string
+                    'choice_label' => 'label',
+                    'label' => 'Rôles',
+                    'multiple' => true,
+                    'expanded' => true,
+                    'choice_attr' => function($responsibility)
+                    {
+                        return ['data-responsibility-description' => $responsibility->getDescription()];
+                    },
+                ])
+                ->add('submit',SubmitType::class, [
+                'label' => 'Changer les informations',
+                'attr' => [
+                    'class' => 'btn btn-outline-primary float-right'
+                    ]
+                ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'app_user';
+    }
+
+}
