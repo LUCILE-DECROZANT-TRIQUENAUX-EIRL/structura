5e2372278725142fcce0167ec4b4fe2458d7ec3f
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-11-16 15:31:54 +0100

    Ref #65: check user input on fax number field

--- public/javascript/People/phone.js
+++ public/javascript/People/phone.js
@@ -182,14 +182,14 @@ $(document).ready(function ($) {
      * --------------------------------------------------
      */
     // Add an animated transition on the help text
-    $('#app_user_homePhoneNumber_help').css({transition : 'all 0.1s ease-in-out'});
+    $('#app_user_workPhoneNumber_help').css({transition : 'all 0.1s ease-in-out'});
 
     // User is deleting things in the input
     // Reset of the helper classes
-    $('[name="app_user[homePhoneNumber]"]').keydown(function (e) {
+    $('[name="app_user[workPhoneNumber]"]').keydown(function (e) {
         if (e.which == 8)
         {
-            regularizeInputHelper($('#app_user_homePhoneNumber_help'));
+            regularizeInputHelper($('#app_user_workPhoneNumber_help'));
             return true;
         }
     });
@@ -250,26 +250,76 @@ $(document).ready(function ($) {
         placeholder: "__ __ __ __ __"
     });
 
+    /*
+     * --------------------------------------------------
+     * --- Configure the checker of workFax phone number ---
+     * --------------------------------------------------
+     */
+    // Add an animated transition on the help text
+    $('#app_user_homeNumber_help').css({transition : 'all 0.1s ease-in-out'});
 
-    $('[name="app_user[workFaxNumber]"]').keypress(function (e) {
-        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+    // User is deleting things in the input
+    // Reset of the helper classes
+    $('[name="app_user[workFaxNumber]"]').keydown(function (e) {
+        if (e.which == 8)
         {
-            return false;
-        }
-        var curchr = this.value.length;
-        var curval = $(this).val();
-        var phonenumber = "";
-        phonenumber = phonenumber + curval;
-        if (curchr == 2 && phonenumber[0] == 0)
-        {
-            $(this).attr('maxlength', '14');
-        }
-        else if (curchr == 2 && phonenumber[0] != 0)
-        {
-            e.preventDefault();
+            regularizeInputHelper($('#app_user_workFaxNumber_help'));
+            return true;
         }
     });
 
+    // User is quitting the input
+    // There is no need anymore of emphasis
+    $('[name="app_user[workFaxNumber]"]').focusout(function () {
+        regularizeInputHelper($('#app_user_workFaxNumber_help'));
+        return true;
+    })
+
+    $('[name="app_user[workFaxNumber]"]').keypress(function (e) {
+        $(this).attr('maxlength', '14');
+
+        // Check if the character is a number, prevent it if true
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+        {
+            emphaseInputHelper($('#app_user_workFaxNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+
+        var characterPressed = String.fromCharCode(e.which);
+        var currentCharacterPosition = this.value.length;
+
+        if (currentCharacterPosition == 0 && characterPressed !== '0')
+        {
+            // User is trying to enter an invalid phone number
+            // Stop the user from writing the phone number
+            emphaseInputHelper($('#app_user_workFaxNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+        else if (currentCharacterPosition == 1 && (characterPressed == 0))
+        {
+            // User is trying to enter an invalid phone number
+            // Stop the user from writing the phone number
+            emphaseInputHelper($('#app_user_workFaxNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+        else if (currentCharacterPosition == 1 && (characterPressed != '0'))
+        {
+            // Reset input helper to its default look
+            regularizeInputHelper($('#app_user_workFaxNumber_help'));
+            return true;
+        }
+        else
+        {
+            // Reset input helper to its default look
+            regularizeInputHelper($('#app_user_workFaxNumber_help'));
+            return true;
+        }
+    });
+
+    // Setup the placeholder in the input
     $('[name="app_user[workFaxNumber]"]').mask('99 99 99 99 99', {
         placeholder: "__ __ __ __ __"
     });
