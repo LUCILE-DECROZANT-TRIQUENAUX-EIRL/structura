94bdd726cdeb602ea1c338a02fcb0b035a9cb9e3
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-11-16 15:20:26 +0100

    Ref #65: check input for home phone

--- public/javascript/People/phone.js
+++ public/javascript/People/phone.js
@@ -97,37 +97,86 @@ $(document).ready(function ($) {
         }
     });
 
-
+    // Setup the placeholder in the input
     $('[name="app_user[cellPhoneNumber]"]').mask('99 99 99 99 99', {
         placeholder: "__ __ __ __ __"
     });
 
-    $('[name="app_user[homePhoneNumber]"]').keypress(function (e) {
-        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+    /*
+     * --------------------------------------------------
+     * --- Configure the checker of home phone number ---
+     * --------------------------------------------------
+     */
+    // Add an animated transition on the help text
+    $('#app_user_homePhoneNumber_help').css({transition : 'all 0.1s ease-in-out'});
+
+    // User is deleting things in the input
+    // Reset of the helper classes
+    $('[name="app_user[homePhoneNumber]"]').keydown(function (e) {
+        if (e.which == 8)
         {
-            return false;
-        }
-        var curchr = this.value.length;
-        var curval = $(this).val();
-        var phonenumber = "";
-        phonenumber = phonenumber + curval;
-        if (curchr == 2
-                && (phonenumber[1] == 1 || phonenumber[1] == 2 || phonenumber[1] == 3 || phonenumber[1] == 4 || phonenumber[1] == 5)
-                && phonenumber[0] == 0)
-        {
-            $(this).attr('maxlength', '14');
-        }
-        else if (curchr == 2 &&
-                (phonenumber[1] != 1 || phonenumber[1] != 2 || phonenumber[1] != 3
-                        || phonenumber[1] != 4 || phonenumber[1] != 5 || phonenumber[0] != 0))
-        {
-            e.preventDefault();
+            regularizeInputHelper($('#app_user_homePhoneNumber_help'));
+            return true;
         }
     });
 
+    // User is quitting the input
+    // There is no need anymore of emphasis
+    $('[name="app_user[homePhoneNumber]"]').focusout(function () {
+        regularizeInputHelper($('#app_user_homePhoneNumber_help'));
+        return true;
+    })
+
+    $('[name="app_user[homePhoneNumber]"]').keypress(function (e) {
+        $(this).attr('maxlength', '14');
+
+        // Check if the character is a number, prevent it if true
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+        {
+            emphaseInputHelper($('#app_user_homePhoneNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+
+        var characterPressed = String.fromCharCode(e.which);
+        var currentCharacterPosition = this.value.length;
+
+        if (currentCharacterPosition == 0 && characterPressed !== '0')
+        {
+            // User is trying to enter an invalid phone number
+            // Stop the user from writing the phone number
+            emphaseInputHelper($('#app_user_homePhoneNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+        else if (currentCharacterPosition == 1 && (characterPressed == 6 || characterPressed == 7 || characterPressed == 0))
+        {
+            // User is trying to enter an invalid phone number
+            // Stop the user from writing the phone number
+            emphaseInputHelper($('#app_user_homePhoneNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+        else if (currentCharacterPosition == 1 && (characterPressed != '6' || characterPressed != '7' || characterPressed == '0'))
+        {
+            // Reset input helper to its default look
+            regularizeInputHelper($('#app_user_homePhoneNumber_help'));
+            return true;
+        }
+        else
+        {
+            // Reset input helper to its default look
+            regularizeInputHelper($('#app_user_homePhoneNumber_help'));
+            return true;
+        }
+    });
+
+    // Setup the placeholder in the input
     $('[name="app_user[homePhoneNumber]"]').mask('99 99 99 99 99', {
         placeholder: "__ __ __ __ __"
     });
+
+
 
     $('[name="app_user[workPhoneNumber]"]').keypress(function (e) {
         if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
