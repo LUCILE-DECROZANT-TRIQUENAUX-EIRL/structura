42c6a4d11f05ebff6925dea90d844e1e6124352d
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-11-16 15:06:58 +0100

    Ref #65: improve UX on cell phone registration - add emphasis on the input helper qhen user make a mistake - remove emphasis when no need anymore

--- public/javascript/People/phone.js
+++ public/javascript/People/phone.js
@@ -1,23 +1,102 @@
 $(document).ready(function ($) {
 
-    $('[name="app_user[cellPhoneNumber]"]').keypress(function (e) {
-        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+    /*
+     * -----------------------------------------
+     * --- Configure the input helpers style ---
+     * -----------------------------------------
+     */
+
+    /**
+     * Add emphasis on the input helper text given in parameters
+     *
+     * @param Object inputHelper the input helper needing emphasis
+     */
+    function emphaseInputHelper(inputHelper) {
+        inputHelper.css({"font-size":"110%"});
+        inputHelper.removeClass('text-muted');
+        inputHelper.addClass('text-primary');
+    }
+
+    /**
+     * Reset the input helper text given in parameters to its original look
+     *
+     * @param Object inputHelper the input helper needing reset
+     */
+    function regularizeInputHelper(inputHelper) {
+        inputHelper.css({"font-size":"80%"});
+        inputHelper.addClass('text-muted');
+        inputHelper.removeClass('text-primary');
+    }
+
+    /*
+     * --------------------------------------------------
+     * --- Configure the checker of cell phone number ---
+     * --------------------------------------------------
+     */
+    // Add an animated transition on the help text
+    $('#app_user_cellPhoneNumber_help').css({transition : 'all 0.1s ease-in-out'});
+
+    // User is deleting things in the input
+    // Reset of the helper classes
+    $('[name="app_user[cellPhoneNumber]"]').keydown(function (e) {
+        if (e.which == 8)
         {
-            return false;
-        }
-        var curchr = this.value.length;
-        var curval = $(this).val();
-        var phonenumber = "";
-        phonenumber = phonenumber + curval;
-        if (curchr == 2 && (phonenumber[1] == 6 || phonenumber[1] == 7) && phonenumber[0] == 0)
-        {
-            $(this).attr('maxlength', '14');
-        }
-        else if (curchr == 2 && (phonenumber[1] != 6 || phonenumber[1] != 7 || phonenumber[0] != 0))
-        {
-            e.preventDefault();
+            regularizeInputHelper($('#app_user_cellPhoneNumber_help'));
+            return true;
         }
     });
+
+    // User is quitting the input
+    // There is no need anymore of emphasis
+    $('[name="app_user[cellPhoneNumber]"]').focusout(function () {
+        regularizeInputHelper($('#app_user_cellPhoneNumber_help'));
+        return true;
+    })
+
+    $('[name="app_user[cellPhoneNumber]"]').keypress(function (e) {
+        $(this).attr('maxlength', '14');
+
+        // Check if the character is a number, prevent it if true
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
+        {
+            emphaseInputHelper($('#app_user_cellPhoneNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+
+        var characterPressed = String.fromCharCode(e.which);
+        var currentCharacterPosition = this.value.length;
+
+        if (currentCharacterPosition == 0 && characterPressed !== '0')
+        {
+            // User is trying to enter an invalid phone number
+            // Stop the user from writing the phone number
+            emphaseInputHelper($('#app_user_cellPhoneNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+        else if (currentCharacterPosition == 1 && (characterPressed == 6 || characterPressed == 7))
+        {
+            // Reset input helper to its default look
+            regularizeInputHelper($('#app_user_cellPhoneNumber_help'));
+            return true;
+        }
+        else if (currentCharacterPosition == 1 && (characterPressed != '6' || characterPressed != '7'))
+        {
+            // User is trying to enter an invalid phone number
+            // Stop the user from writing the phone number
+            emphaseInputHelper($('#app_user_cellPhoneNumber_help'));
+            e.preventDefault();
+            return false;
+        }
+        else
+        {
+            // Reset input helper to its default look
+            regularizeInputHelper($('#app_user_cellPhoneNumber_help'));
+            return true;
+        }
+    });
+
 
     $('[name="app_user[cellPhoneNumber]"]').mask('99 99 99 99 99', {
         placeholder: "__ __ __ __ __"
@@ -118,5 +197,4 @@ $(document).ready(function ($) {
         $('[name="app_user[workPhoneNumber]"]').unmask();
         $('[name="app_user[workFaxNumber]"]').unmask();
     });
-
 });
