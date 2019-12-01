cabb90d6bb51562540246a171db1c5eda10c1c97
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-12-01 11:13:06 +0100

    Ref #94: add more memberships to the members in fixtures

--- src/DataFixtures/MembershipFixtures.php
+++ src/DataFixtures/MembershipFixtures.php
@@ -55,39 +55,15 @@ class MembershipFixtures extends Fixture implements FixtureGroupInterface
         $paymentTypeCheck = $paymentTypeRepository->findOneBy(['label' => 'Chèque']);
         $paymentTypeHelloAsso = $paymentTypeRepository->findOneBy(['label' => 'HelloAsso']);
 
-
-        // Retreiving adherent.e.s from DB
-        $peopleRepository = $manager->getRepository(People::class);
-
-        // adhe1
-        $peopleAdherentE1 = $peopleRepository->findOneBy([
-            'firstName' => 'Jeanne',
-            'lastName' => 'Vérany'
-        ]);
-        // adhe2
-        $peopleAdherentE2 = $peopleRepository->findOneBy([
-            'firstName' => 'Arlette',
-            'lastName' => 'Maurice'
-        ]);
-        // adhe3
-        $peopleAdherentE3 = $peopleRepository->findOneBy([
-            'firstName' => 'Ladislas',
-            'lastName' => 'Bullion'
-        ]);
-
-        // -- Payments -- //
-
-        // First payment
+        // First payment (check)
         $paymentAdhesionCheque50 = new Payment();
-
         $paymentAdhesionCheque50->setAmount(50);
         $paymentAdhesionCheque50->setType($paymentTypeCheck);
 
         $manager->persist($paymentAdhesionCheque50);
 
-        // Second payment
+        // Second payment (hello asso)
         $paymentAdhesionHelloAsso30 = new Payment();
-
         $paymentAdhesionHelloAsso30->setAmount(30);
         $paymentAdhesionHelloAsso30->setType($paymentTypeHelloAsso);
         $paymentAdhesionHelloAsso30->setDateReceived(new \DateTime());
@@ -95,15 +71,48 @@ class MembershipFixtures extends Fixture implements FixtureGroupInterface
 
         $manager->persist($paymentAdhesionHelloAsso30);
 
-        // Third payment
-        $paymentAdhesionHelloAsso20 = new Payment();
+        // Third payment (cash)
+        $paymentAdhesionCash20 = new Payment();
+        $paymentAdhesionCash20->setAmount(20);
+        $paymentAdhesionCash20->setType($paymentTypeCash);
+        $paymentAdhesionCash20->setDateReceived(new \DateTime());
+        $paymentAdhesionCash20->setDateCashed(new \DateTime());
 
-        $paymentAdhesionHelloAsso20->setAmount(20);
-        $paymentAdhesionHelloAsso20->setType($paymentTypeHelloAsso);
-        $paymentAdhesionHelloAsso20->setDateReceived(new \DateTime());
-        $paymentAdhesionHelloAsso20->setDateCashed(new \DateTime());
+        $manager->persist($paymentAdhesionCash20);
 
-        $manager->persist($paymentAdhesionHelloAsso20);
+        // Fourth payment (card)
+        $paymentAdhesionCard20 = new Payment();
+        $paymentAdhesionCard20->setAmount(20);
+        $paymentAdhesionCard20->setType($paymentTypeCard);
+        $paymentAdhesionCard20->setDateReceived(new \DateTime());
+        $paymentAdhesionCard20->setDateCashed(new \DateTime());
+
+        $manager->persist($paymentAdhesionCard20);
+
+        // Fifth payment (transfer)
+        $paymentAdhesionTransfer20 = new Payment();
+        $paymentAdhesionTransfer20->setAmount(20);
+        $paymentAdhesionTransfer20->setType($paymentTypeCard);
+        $paymentAdhesionTransfer20->setDateReceived(new \DateTime());
+        $paymentAdhesionTransfer20->setDateCashed(new \DateTime());
+
+        $manager->persist($paymentAdhesionTransfer20);
+
+        // Generate several other payments
+        $otherPayments = [];
+        $i = 1;
+        do
+        {
+            $paymentAdhesionCheque20 = new Payment();
+            $paymentAdhesionCheque20->setAmount(20);
+            $paymentAdhesionCheque20->setType($paymentTypeCheck);
+
+            $manager->persist($paymentAdhesionCheque20);
+            $otherPayments[] = $paymentAdhesionCheque20;
+
+            $i++;
+        }
+        while ($i === 15);
 
         // Date managment
         $plusOneYear = new \DateInterval('P1Y');
@@ -118,8 +127,20 @@ class MembershipFixtures extends Fixture implements FixtureGroupInterface
         $lastYear = clone $now;
         $lastYear->add($removeOneYear);
 
+        // Retreiving adherent.e.s from DB
+        $peopleRepository = $manager->getRepository(People::class);
+        $people = $peopleRepository->findWithNoMembership();
+        $members = [];
+        foreach ($people as $individual)
+        {
+            if (substr($individual->getUser()->getUsername(), 0, 4) === 'adhe')
+            {
+                $members[]['member'] = $individual;
+            }
+        }
+
         // -- Memberships -- //
-        // Normal 1
+        // First regular
         $membershipNormal1 = new Membership();
 
         $membershipNormal1
@@ -131,14 +152,14 @@ class MembershipFixtures extends Fixture implements FixtureGroupInterface
 
         $manager->persist($membershipNormal1);
 
-        // Normal 2
+        // Second regular
         $membershipNormal2 = new Membership();
 
         $membershipNormal2
             ->setAmount(20)
             ->setDateStart($lastYear)
             ->setDateEnd($now)
-            ->setPayment($paymentAdhesionHelloAsso20)
+            ->setPayment($paymentAdhesionCash20)
             ->setType($MembershipTypeRegular);
 
         $manager->persist($membershipNormal2);
@@ -154,16 +175,62 @@ class MembershipFixtures extends Fixture implements FixtureGroupInterface
 
         $manager->persist($membershipFamily);
 
-        // Adding the memberships to the people
-        $peopleAdherentE1->addMembership($membershipNormal2);
-        $peopleAdherentE2->addMembership($membershipFamily);
-        $peopleAdherentE3->addMembership($membershipNormal1);
-        $peopleAdherentE3->addMembership($membershipFamily);
+        // Adding memberships to all the members
+        foreach ($members as $index => $member)
+        {
+            // regular membership
+            $membershipRegular = new Membership();
+            // Card payment generated for this membership
+            $paymentAdhesionCard20 = new Payment();
+            $paymentAdhesionCard20->setAmount(20);
+            $paymentAdhesionCard20->setType($paymentTypeCard);
+            $paymentAdhesionCard20->setDateReceived(new \DateTime());
+            $paymentAdhesionCard20->setDateCashed(new \DateTime());
 
-        // people persist
-        $manager->persist($peopleAdherentE1);
-        $manager->persist($peopleAdherentE2);
-        $manager->persist($peopleAdherentE3);
+            // Create ans associate active and inactive memberships evenly on members
+            if ($index % 2 == 0)
+            {
+                // Active membership
+                $membershipRegular
+                    ->setAmount(20)
+                    ->setDateStart($now)
+                    ->setDateEnd($inOneYear)
+                    ->setPayment($paymentAdhesionCard20)
+                    ->setType($MembershipTypeRegular);
+            }
+            else
+            {
+                // Inactive membership
+                $membershipRegular
+                    ->setAmount(20)
+                    ->setDateStart($lastYear)
+                    ->setDateEnd($now)
+                    ->setPayment($paymentAdhesionCard20)
+                    ->setType($MembershipTypeRegular);
+            }
+            $manager->persist($membershipRegular);
+            $members[$index]['membership'] = $membershipRegular;
+        }
+
+        // Adding other memberships to the people
+        $members[0]['member']->addMembership($membershipNormal2);
+        $members[1]['member']->addMembership($membershipFamily);
+        $members[2]['member']->addMembership($membershipNormal1);
+        $members[2]['member']->addMembership($membershipFamily);
+        // Save the memberships of the members
+        $manager->persist($members[0]['member']);
+        $manager->persist($members[1]['member']);
+        $manager->persist($members[2]['member']);
+
+        // Add a little more memberships to the people
+        // using previously created memberships
+        foreach ($members as $index => $memberAndMembership)
+        {
+            $member = $memberAndMembership['member'];
+            $membership = $memberAndMembership['membership'];
+            $member->addMembership($membership);
+            $manager->persist($member);
+        }
 
         // Final flush
         $manager->flush();
