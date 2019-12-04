96a69755b10de6c7522790ab81925485acb814e9
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-12-04 13:05:55 +0100

    MINOR: remove useless code

--- src/Repository/DonationRepository.php
+++ src/Repository/DonationRepository.php
@@ -19,32 +19,4 @@ class DonationRepository extends ServiceEntityRepository
         parent::__construct($registry, Donation::class);
     }
 
-    // /**
-    //  * @return Donation[] Returns an array of Donation objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('d.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Donation
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
