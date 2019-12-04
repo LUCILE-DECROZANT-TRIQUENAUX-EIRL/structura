a9452b9cda9053d0e5462fbfc0a9db9b41c2f405
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-12-04 15:58:54 +0100

    Ref #105: add a class on opened rows to change its backgroung color

--- public/javascript/Home/index.js
+++ public/javascript/Home/index.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    
+    $(function () {
+        $('#people-waiting-send-up').DataTable().on('responsive-display', function (e, datatable, row, showHide, update) {
+            if (showHide) {
+                $(datatable.row(row.index()).node()).addClass('row-opened');
+                $('[data-toggle="tooltip"]').tooltip()
+            } else {
+                $(datatable.row(row.index()).node()).removeClass('row-opened');
+            }
+        });
+    });
 });
