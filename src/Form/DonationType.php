cbbbac22906b58c1932d690cadfde076ffa80335
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2019-12-11 22:09:27 +0100

    Ref #106: add donation type

--- src/Form/DonationType.php
+++ src/Form/DonationType.php
@@ -3,7 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Donation;
+use App\Entity\People;
+use App\Entity\Payment;
+use App\Entity\PaymentType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +18,39 @@ class DonationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('amount')
-            ->add('donation_date')
-            ->add('payment')
-            ->add('donator')
+                ->add('amount', MoneyType::class)
+                ->add('payment_type', EntityType::class, [
+                    // looks for choices from this entity
+                    'class' => PaymentType::class,
+                    // uses the label property as the visible option string
+                    'choice_label' => 'label',
+                    'multiple' => false,
+                    'expanded' => false,
+                ])
+                ->add('donator', EntityType::class, [
+                    // looks for choices from this entity
+                    'class' => People::class,
+                    // uses firstname and lastname as the visible option string
+                    'choice_label' => function($people) {
+                        return $people->getFirstName() . ' ' . $people->getLastName();
+                    },
+                    'multiple' => false,
+                    'expanded' => false,
+                ])
+                ->add('donation_date', DateType::class, [
+                    'widget' => 'single_text',
+                ])
+                ->add('cashed_date', DateType::class, [
+                    'widget' => 'single_text',
+                    'required' => false,
+                ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Donation::class,
+            'data_class' => 'App\FormDataObject\UpdateDonationFDO',
         ]);
     }
 }
