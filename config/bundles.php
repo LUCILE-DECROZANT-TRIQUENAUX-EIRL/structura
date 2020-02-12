d75c802d3323f1336151458926d4239dd9d0cd27
yann-butscher-eirl <yann.butscher.eirl@gmail.com>
2020-02-12 16:11:32 +0100

    Ref #109 : Added Translatable Doctrine Bundle for database translation.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'travis' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
