6b7ead63fa068d7773ef6f8373e2bb37deb995b2
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2020-05-04 17:14:14 +0200

    Ref #175: add icons to show that the lines are expandables

--- public/javascript/Home/index.js
+++ public/javascript/Home/index.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
     $(function () {
         $('#people-waiting-send-up').DataTable().on('responsive-display', function (e, datatable, row, showHide, update) {
+            var row = $(datatable.row(row.index()).node());
             if (showHide) {
-                $(datatable.row(row.index()).node()).addClass('row-opened');
-                $('[data-toggle="tooltip"]').tooltip()
+                // Add class to show the row is expanded
+                row.addClass('row-opened');
+                // Change tooltip
+                var openedRowTitle = row.data('title-openened')
+                row.attr('title', openedRowTitle)
+                        .tooltip('_fixTitle')
+                        .tooltip('show');
+                // Set the icon as chevron up
+                row.find('.expand-icon > i').removeClass('ion-ios-arrow-down')
+                row.find('.expand-icon > i').addClass('ion-ios-arrow-up')
             } else {
-                $(datatable.row(row.index()).node()).removeClass('row-opened');
+                // Add class to show the row is collapsed
+                row.removeClass('row-opened');
+                // Change tooltip
+                var closedRowTitle = row.data('title-closed')
+                row.attr('title', closedRowTitle)
+                        .tooltip('_fixTitle')
+                        .tooltip('show');
+                // Set the icon as chevron down
+                row.find('.expand-icon > i').removeClass('ion-ios-arrow-up')
+                row.find('.expand-icon > i').addClass('ion-ios-arrow-down')
             }
         });
     });
