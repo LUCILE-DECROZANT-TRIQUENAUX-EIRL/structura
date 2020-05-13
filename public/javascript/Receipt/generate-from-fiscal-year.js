36e73c5f1a9642db8ff53b4c7ee47733cbf2a6de
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2020-05-13 19:31:13 +0200

    Ref #178: disable form even if user refresh thhe page with a value already selected

--- public/javascript/Receipt/generate-from-fiscal-year.js
+++ public/javascript/Receipt/generate-from-fiscal-year.js
@@ -1,21 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // if there is a PDF generation not ended, disable the form
+    // and display an explanation message
+    if ($('#generate_tax_receipt_from_fiscal_year_fiscalYear option:selected').data('is-under-generation')) {
+        disableForm();
+    }
     // On fiscal year selection,
     // if there is a PDF generation not ended, disable the form
     // and display an explanation message
-    $('#generate_tax_receipt_from_fiscal_year_fiscalYear').change(function() {
+    $('#generate_tax_receipt_from_fiscal_year_fiscalYear').change(function () {
         if ($(this).find('option:selected').data('is-under-generation')) {
-            // Setup and show the message
-            $('#fiscal-year').text($(this).find('option:selected').text());
-            $('#message-generation-disabled').removeClass('d-none');
-            // Disable the submit button
-            $('#from-fiscal-year-submit-button').prop('disabled', true);
-            $('#from-fiscal-year-submit-button').addClass('disabled');
+            disableForm();
         } else {
-            // Hide the message
-            $('#message-generation-disabled').addClass('d-none');
-            // Enable the button
-            $('#from-fiscal-year-submit-button').prop('disabled', false);
-            $('#from-fiscal-year-submit-button').removeClass('disabled');
+            enableForm();
         }
     });
 });
+
+function disableForm() {
+    // Setup and show the message
+    $('#fiscal-year').text($(this).find('option:selected').text());
+    $('#message-generation-disabled').removeClass('d-none');
+    // Disable the submit button
+    $('#from-fiscal-year-submit-button').prop('disabled', true);
+    $('#from-fiscal-year-submit-button').addClass('disabled');
+}
+
+function enableForm() {
+    // Hide the message
+    $('#message-generation-disabled').addClass('d-none');
+    // Enable the button
+    $('#from-fiscal-year-submit-button').prop('disabled', false);
+    $('#from-fiscal-year-submit-button').removeClass('disabled');
+}
