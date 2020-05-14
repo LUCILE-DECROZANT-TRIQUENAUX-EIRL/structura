afc05d4fb9c6778da7d2c0b2e76b58153a80bf40
yann-butscher-eirl <yann.butscher.eirl@gmail.com>
2020-05-14 11:58:19 +0200

    MINOR : Removed unused controller action

--- src/Controller/ReceiptController.php
+++ src/Controller/ReceiptController.php
@@ -160,43 +160,6 @@ class ReceiptController extends AbstractController
     }
 
     /**
-     * @return void
-     * @param Request $request The request.
-     * @Route("/pdf", name="pdf", requirements={"_locale"="en|fr"})
-     * @Security("is_granted('ROLE_GESTION')")
-     */
-    public function pdfAction(Request $request)
-    {
-        // for testing purpose, get one payment from the database
-        $em = $this->getDoctrine()->getManager();
-        $payment = $em->getRepository(Payment::class)
-                ->findOneById(1);
-
-        // Render the PDF content using a twig template
-        $htmlNeedingConversion = $this->renderView('PDF/Receipt/_tax_receipt.html.twig', [
-            'orderCode' => '2014_812',
-            'payment' => $payment,
-            'receiptGenerationDate' => new \DateTime('2014-06-20 00:00:00'),
-        ]);
-
-        // Object instantiation
-        $dompdf = new Dompdf();
-
-        $dompdf->loadHtml($htmlNeedingConversion);
-
-        // Set the page format and orientation
-        $dompdf->setPaper('A4', 'portrait');
-
-        $dompdf->render();
-
-        $dompdf->stream('tax_receipt.pdf', [
-            'Attachment' => false // If set to true, force download, else give a preview
-        ]);
-
-        return true;
-    }
-
-    /**
      * @return BinaryFileResponse
      * @param Request $request The request.
      * @Route("/download-grouped-pdf/{id}", name="download_grouped_receipts_pdf", requirements={"_locale"="en|fr"})
