4aec3401eb524f834d2f9e3f4c783f7dfd383af4
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2020-06-23 22:38:05 +0200

    Ref #199: add form to upload asso logo

--- src/Controller/AssociationController.php
+++ src/Controller/AssociationController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Association;
 use App\Form\AssociationType;
+use App\Form\AssociationLogoType;
+use App\FormDataObject\AssociationLogoFDO;
 use App\FormDataObject\AssociationNameFDO;
 use App\Repository\AssociationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -62,6 +64,47 @@ class AssociationController extends AbstractController
         }
 
         return $this->render('Association/edit.html.twig', [
+            'association' => $association,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/upload-logo", name="association_upload_logo", methods={"GET","POST"})
+     */
+    public function uploadLogoAction(Request $request) {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // use a FDO to only edit the name of the association
+        $associationLogoFDO = new AssociationLogoFDO();
+        $association = $entityManager->getRepository(Association::class)->findOneById(1);
+        if (empty($association)) {
+            $association = new Association();
+        }
+
+        $form = $this->createForm(AssociationLogoType::class, $associationLogoFDO);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $logo = $associationLogoFDO->getLogo();
+            $name = 'logo.' . $logo->guessExtension();
+            $uploadDirectory = $this->getParameter('kernel.project_dir') . '/public/images/uploads';
+            $logo->move(
+                    $uploadDirectory,
+                    $name
+            );
+
+            $association->setLogoFilename('images/uploads/' . $name);
+            $entityManager->persist($association);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success', 'Le logo a bien été téléversé.'
+            );
+            return $this->redirectToRoute('association_index');
+        }
+
+        return $this->render('Association/edit-logo.html.twig', [
             'association' => $association,
             'form' => $form->createView(),
         ]);
