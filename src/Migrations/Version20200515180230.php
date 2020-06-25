dba763d02b2b6a066e3a427a730c4bb6a19b4e1b
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2020-06-25 14:27:12 +0200

    Ref #192: fix the migrations

--- src/Migrations/Version20200515180230.php
+++ src/Migrations/Version20200515180230.php
@@ -7,10 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20200624215522 extends AbstractMigration
+final class Version20200515180230 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -19,7 +16,6 @@ final class Version20200624215522 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE receipt CHANGE fiscal_year year INT NOT NULL');
@@ -27,7 +23,6 @@ final class Version20200624215522 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE receipt CHANGE year fiscal_year INT NOT NULL');
