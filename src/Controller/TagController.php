015de11433a360394a42d6c647b170c9ed509664
Yann-BUTSCHER-EIRL <yann.butscher.eirl@gmail.com>
2020-09-11 00:17:28 +0200

    Ref #230 : Added a new Tag page and the tag generation logic.

---
+++ src/Controller/TagController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\People;
+use App\Service\TagService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(path="/{_locale}/tag", requirements={"_locale"="en|fr"})
+ */
+class TagController extends AbstractController
+{
+    /**
+     * @Route("/", name="tag_index", requirements={"_locale"="en|fr"})
+     * @Security("is_granted('ROLE_GESTION')")
+     */
+    public function index()
+    {
+        // Entity manager
+        $em = $this->getDoctrine()->getManager();
+        $people = $em->getRepository(People::class)->findBy([], ['lastName' => 'ASC']);
+
+        return $this->render('Tag/index.html.twig', [
+            'people' => $people,
+        ]);
+    }
+
+    /**
+     * @Route("/generate", name="tag_generate", requirements={"_locale"="en|fr"})
+     * @Security("is_granted('ROLE_GESTION')")
+     */
+    public function generate(TagService $tagService)
+    {
+        // Entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        // $people = $em->getRepository(People::class)->findAll();
+        $people = $em->getRepository(People::class)->findBy([], ['lastName' => 'ASC']);
+
+        return $tagService->generateTagsPdf($people, 'etiquettes', true, true);
+    }
+}
