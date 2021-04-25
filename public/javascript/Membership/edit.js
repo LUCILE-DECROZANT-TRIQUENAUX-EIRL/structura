e32c281acbab9a22cebe3562b3fb423409381b6a
Yann-BUTSCHER-EIRL <yann.butscher.eirl@gmail.com>
2021-04-25 23:17:11 +0200

    Fixed the validation error that occured because of empty payer list on page loading.

--- public/javascript/Membership/edit.js
+++ public/javascript/Membership/edit.js
@@ -11,6 +11,10 @@ $(document).ready(function() {
 
     let membershipTypeId = $('#app_membership_membershipType').val();
 
+    // Empty the payer selection list but keep the placeholder
+    $('#app_membership_payer option').not(':first').remove();
+    $('#check-issuer option').not(':first').remove();
+
     getMembershipType(membershipTypeId).then(function(value) {
         // Manually reseting the checkboxes
         $('input[type=checkbox]').prop('checked', false);
@@ -21,7 +25,7 @@ $(document).ready(function() {
             let selectedPeopleOption = $('#app_membership_newMember option[value="' + selectedPeopleId + '"]');
             let selectedPeopleName = selectedPeopleOption.html();
 
-            selectPeople(selectedPeopleId, selectedPeopleName, false);
+            selectPeople(selectedPeopleId, selectedPeopleName, true);
         });
     });
 });
