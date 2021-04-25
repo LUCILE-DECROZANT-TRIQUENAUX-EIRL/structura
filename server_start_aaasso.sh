b9436d4fe8f38cc4bc3408901dbee1aa6b5b0673
Yann-BUTSCHER-EIRL <yann.butscher.eirl@gmail.com>
2021-04-25 22:12:15 +0200

    MINOR : Changed the way we launch the messenger worker.

--- server_start_aaasso.sh
+++ server_start_aaasso.sh
@@ -272,7 +272,8 @@ fi
 cp ".env.local_for_${env}" .env.local
 
 # Launching the messenger process that consume messages in a new terminal
-x-terminal-emulator -e symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async -vv
+# x-terminal-emulator -e symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async -vv
+x-terminal-emulator -e symfony run --watch=config,src,templates,vendor symfony console messenger:consume async -vv
 
 # Starting the symfony server with the newly created env file
 symfony self:update --yes
