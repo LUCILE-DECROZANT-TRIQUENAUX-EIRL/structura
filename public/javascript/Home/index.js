4fbe9fa719832fd333cc7dde08434245cf8c19d9
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2022-02-07 18:39:02 +0100

    Add missing commas

--- public/javascript/Home/index.js
+++ public/javascript/Home/index.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     $(function () {
         /* Displaying revenues graph */
         var chart = $('#revenues-chart');
-        var labels = ['','','','','','','','','','','','']
-        var donationRevenues = [0,0,0,0,0,0,0,0,0,0,0,0]
-        var membershipRevenues = [0,0,0,0,0,0,0,0,0,0,0,0]
-        var cumulatedRevenues = [0,0,0,0,0,0,0,0,0,0,0,0]
+        var labels = ['','','','','','','','','','','',''];
+        var donationRevenues = [0,0,0,0,0,0,0,0,0,0,0,0];
+        var membershipRevenues = [0,0,0,0,0,0,0,0,0,0,0,0];
+        var cumulatedRevenues = [0,0,0,0,0,0,0,0,0,0,0,0];
         $.ajax({
             url: chart.data('url-data'),
             datatype: 'json'
@@ -66,24 +66,24 @@ $(document).ready(function () {
                 // Add class to show the row is expanded
                 row.addClass('row-opened');
                 // Change tooltip
-                var openedRowTitle = row.data('title-openened')
+                var openedRowTitle = row.data('title-openened');
                 row.attr('title', openedRowTitle)
                         .tooltip('_fixTitle')
                         .tooltip('show');
                 // Set the icon as chevron up
-                row.find('.expand-icon > i').removeClass('ion-ios-arrow-down')
-                row.find('.expand-icon > i').addClass('ion-ios-arrow-up')
+                row.find('.expand-icon > i').removeClass('ion-ios-arrow-down');
+                row.find('.expand-icon > i').addClass('ion-ios-arrow-up');
             } else {
                 // Add class to show the row is collapsed
                 row.removeClass('row-opened');
                 // Change tooltip
-                var closedRowTitle = row.data('title-closed')
+                var closedRowTitle = row.data('title-closed');
                 row.attr('title', closedRowTitle)
                         .tooltip('_fixTitle')
                         .tooltip('show');
                 // Set the icon as chevron down
-                row.find('.expand-icon > i').removeClass('ion-ios-arrow-up')
-                row.find('.expand-icon > i').addClass('ion-ios-arrow-down')
+                row.find('.expand-icon > i').removeClass('ion-ios-arrow-up');
+                row.find('.expand-icon > i').addClass('ion-ios-arrow-down');
             }
         });
     });
